8063022e434914b8deba4ce5dbc7174081bd5524
Julen <101423308+Julen384@users.noreply.github.com>
2022-07-27 19:27:06 +0200

    Clase de iniciación a Tipos Js como repaso

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-
 //Éste es el mensaje de bienvenida.
 console.log("Ésta es la puerta de entrada al proyecto.")
+
+/* 
+ÉSTE
+ES
+UN
+COMENTARIO
+MULTILÍNEA
+ */
 
 //Éste va a ser el mensaje que se envía para despedirse.
 console.log("Adiós")
---
+++ iniciacion-tipos.js
@@ -0,0 +1,26 @@
+// Tipos Primitivos
+
+// Number
+4;
+
+// String
+"Hola Mundo";
+'Hola Mundo';
+`Hola Mundo`;
+
+// Booleanos
+true;
+false;
+
+// Null o Undefined
+null;
+undefined;
+
+// Null, Undefined, False, 0 ==> TODAS SON FALSY
+console.log(null === undefined)
+
+if(true){
+    console.log("Cumple")
+} else{
+    console.log("No Cumple")
+}
