257d2ae163feb3d1dd4014e20c034bf39437c11c
Julen <101423308+Julen384@users.noreply.github.com>
2022-08-29 11:17:33 +0200

    first commit

---
+++ iniciacion-variables.js
@@ -0,0 +1,28 @@
+var variable;
+let variableLet;
+
+// const constante;
+const constante = "Hola, soy una Constante";
+console.log(constante);
+
+var a = 1;
+console.log(a);
+
+a = 4;
+console.log(a); 
+
+let b = 3
+console.log(b);
+
+b = 5;
+console.log(b);
+
+
+
+
+
+var variable = "Hola soy una variable VAR";
+
+///////////////////////////
+
+typeof 1
